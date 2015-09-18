7754639a24b37c0ae66108fc6845f09a6a4702b0
nisidazza <nisida.azzalini@gmail.com>
2015-09-18 09:49:19 +0200

    prova

---
+++ prova/prova/prova/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
